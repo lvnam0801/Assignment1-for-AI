15cb2d2a3d010ac3b50a2ac79b1c8f31e2271f79
lvnam0801 <lvnam0801@gmail.com>
2022-11-12 22:57:08 +0700

    Config test

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ Genertic argument
 "Number Crossover"
 NUMBER_LOOP = 100
 "Number Indivudal in Population"
-POPULATION_SIZE = 1
+POPULATION_SIZE = 10
 "The rate to has a mutant individual"
 MUTATE_RATE = 100
